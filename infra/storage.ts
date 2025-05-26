e3a2c31804f649a642a36074228df12c315520a7
scottellis64 <j.scott.ellis64@gmail.com>
2025-05-26 15:06:33 -0400

    Added stripe integration

--- infra/storage.ts
+++ infra/storage.ts
@@ -1,5 +1,6 @@
 // Create an S3 bucket
 export const bucket = new sst.aws.Bucket("Uploads");
+export const secret = new sst.Secret("StripeSecretKey");
 
 // Create the DynamoDB table
 export const table = new sst.aws.Dynamo("Notes", {
