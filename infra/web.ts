315cc124394f9a7584a109ac448b38e7ae238c0d
scottellis64 <j.scott.ellis64@gmail.com>
2025-05-27 13:15:46 -0400

    Added production forwarding

--- infra/web.ts
+++ infra/web.ts
@@ -10,6 +10,13 @@ export const frontend = new sst.aws.StaticSite("Frontend", {
     output: "dist",
     command: "npm run build",
   },
+  domain:
+  $app.stage === "production"
+    ? {
+        name: "fretstop.com",
+        redirects: ["www.fretstop.com"],
+      }
+    : undefined,
   environment: {
     VITE_REGION: region,
     VITE_API_URL: api.url,
