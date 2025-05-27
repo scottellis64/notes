0be510d2650873815d3eede3b266902cef56756a
scottellis64 <j.scott.ellis64@gmail.com>
2025-05-27 14:12:18 -0400

    removed redirect logic that seemed to be messing with deployment

--- packages/core/src/billing/index.ts
+++ packages/core/src/billing/index.ts
@@ -1,6 +1,6 @@
 export module Billing {
-    export function compute(storage: number) {
-      const rate = storage <= 10 ? 4 : storage <= 100 ? 2 : 1;
-      return rate * storage * 100;
-    }
+  export function compute(storage: number) {
+    const rate = storage <= 10 ? 4 : storage <= 100 ? 2 : 1;
+    return rate * storage * 100;
   }
+}
